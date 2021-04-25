6bb7e09f1293f2639974c4d6d77dbb4e7a045fba
andrew.tiema <andrew.tiema@ajiraconnect.com>
2021-04-25 17:55:50 +0300

    initialCommit

---
+++ database/migrations/2021_04_25_144158_create_enquiries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnquiriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enquiries', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('car_id');
+            $table->string('date');
+            $table->string('return_date');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enquiries');
+    }
+}
---
+++ database/migrations/2021_04_25_144440_create_hires_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHiresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hires', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('enquiry_id');
+            $table->string('car_id');
+            $table->string('date');
+            $table->string('returned_date');
+            $table->string('status');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hires');
+    }
+}
