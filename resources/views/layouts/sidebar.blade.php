2881c65865fa7d13eb16593e3facdc463f4cdff6
andrew.tiema <andrew.tiema@ajiraconnect.com>
2021-04-26 05:07:14 +0300

    theme

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,84 @@
+<div class="deznav-scroll">
+    <div class="main-profile">
+        <img src="{{asset('assets/images/Untitled-1.jpg')}}" alt="">
+        <a href="javascript:void(0);"><i class="fa fa-cog" aria-hidden="true"></i></a>
+        <h5 class="mb-0 fs-20 text-black "><span class="font-w400">Hello,</span> Marquez</h5>
+        <p class="mb-0 fs-14 font-w400">marquezzzz@mail.com</p>
+    </div>
+    <ul class="metismenu" id="menu">
+        <li><a class="ai-icon" href="#" aria-expanded="false">
+                <i class="flaticon-144-layout"></i>
+                <span class="nav-text">Dashboard</span>
+            </a>
+        </li>
+        <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                <i class="flaticon-077-menu-1"></i>
+                <span class="nav-text">Enquiries</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="./app-profile.html">All Enquiry</a></li>
+                <li><a href="./post-details.html">Add Enquiry</a></li>
+            </ul>
+        </li>
+        <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                <i class="flaticon-061-puzzle"></i>
+                <span class="nav-text">Reservations</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="./chart-flot.html">Add Reservation</a></li>
+                <li><a href="./chart-morris.html">All Reservations</a></li>
+            </ul>
+        </li>
+        <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                <i class="flaticon-003-diamond"></i>
+                <span class="nav-text">Cars</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="./ui-accordion.html">Add Cars</a></li>
+                <li><a href="./ui-alert.html">All Cars</a></li>
+                <li><a href="./ui-badge.html">Add Car Model</a></li>
+                <li><a href="./ui-button.html">All Car Models</a></li>
+            </ul>
+        </li>
+        <li>
+            <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                <i class="flaticon-053-heart"></i>
+                <span class="nav-text">Hire</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="./uc-select2.html">All Hired</a></li>
+                <li><a href="./uc-nestable.html">Returned</a></li>
+                <li><a href="./uc-noui-slider.html">Hire Report</a></li>
+            </ul>
+        </li>
+
+        <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                <i class="flaticon-003-diamond"></i>
+                <span class="nav-text">Payments</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="./ui-accordion.html">Initiate Payment</a></li>
+                <li><a href="./ui-alert.html">All Payments</a></li>
+            </ul>
+        </li>
+
+        <li>
+            <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                <i class="flaticon-003-diamond"></i>
+                <span class="nav-text">Users</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="./ui-accordion.html">All Users</a></li>
+                <li><a href="./ui-alert.html">Add User</a></li>
+                <li><a href="./ui-alert.html">Block User</a></li>
+            </ul>
+        </li>
+
+        <li>
+            <a href="./page-lock-screen.html">
+                <i class="flaticon-003-logout"></i>
+                <span class="nav-text">Sign Out</span>
+            </a>
+        </li>
+    </ul>
+</div>
