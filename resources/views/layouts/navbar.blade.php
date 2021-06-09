648c54cccee6fce7dc721553f6c936221b797508
andrew.tiema <andrewtiema@gmail.com>
2021-06-09 08:09:09 +0300

    fix

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -19,7 +19,7 @@
                 <!-- User Account: style can be found in dropdown.less -->
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="{{\Illuminate\Support\Facades\Auth::user()->photo}}" class="user-image" alt="User Image">
+{{--                        <img src="{{\Illuminate\Support\Facades\Auth::user()->photo}}" class="user-image" alt="User Image">--}}
                         <span class="hidden-xs">{{\Illuminate\Support\Facades\Auth::user()->name}}</span>
                     </a>
                     <ul class="dropdown-menu">
